9dd00143865670680394a93f4510d7d8e7b2765a
Weronika Wdowiak <weronika.wdowiak56@gmail.com>
2019-02-24 17:13:08 +0100

    initial commit

---
+++ features_canvas/features.js
@@ -0,0 +1,12 @@
+'use strict'
+let can1 = document.getElementById('canvas1');
+let can2 = document.getElementById('canvas2');
+let can3 = document.getElementById('canvas3');
+
+window.addEventListener('load', init)
+
+function init(){
+    drawCan1();
+    drawCan2();
+    drawCan3();
+}
